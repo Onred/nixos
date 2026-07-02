c1675ca7b9cc9c4bd96f4137727951a8ab166911
Onred <jared@onred.com>
2026-07-03 05:04:31 +0900

    feat: add update-flake script

---
+++ modules/update-flake.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    (writeShellApplication {
+      name = "flake-update";
+      runtimeInputs = [ git nix ];
+      text = ''
+        cd ~/nixos || exit 1
+
+        sudo -v
+
+        echo "Updating flake inputs..."
+        nix flake update
+
+        # Check if flake.lock actually changed
+        if ! git diff --quiet flake.lock; then
+          echo "flake.lock changed — building..."
+          nh os boot .
+
+          echo "Committing flake.lock…"
+          git add flake.lock
+          git commit -m "chore: update flake.lock"
+        else
+          echo "No changes in flake.lock."
+        fi
+      '';
+    })
+  ];
+}
