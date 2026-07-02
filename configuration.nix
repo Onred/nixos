08e36936cbcfc09fd14ad5e24d5b660804078120
Onred <jared@onred.com>
2026-07-03 05:04:31 +0900

    feat: add update-flake script

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
     ./modules/packages.nix # System and user packages
     ./modules/sunshine # Sunshine game streaming server
     ./modules/update-check.nix # Update notification service
+    ./modules/flake-update.nix # Flake update and rebuild script
   ];
 
   # Nix
